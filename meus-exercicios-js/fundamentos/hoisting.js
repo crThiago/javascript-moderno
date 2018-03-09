ba5befe90e2aefa04705f00eafc7e1f84d018cc6
crThiago <thiago_8777@hotmail.com>
2018-03-09 01:03:12 -0300

    Controle de varivavel

---
+++ meus-exercicios-js/fundamentos/hoisting.js
@@ -0,0 +1,17 @@
+console.log('a = ', a);
+var a = 2;
+console.log('a = ', a);
+
+function teste() {
+    console.log('a = ', a);
+    var a = 2;
+    console.log('a = ', a);
+}
+
+teste();
+
+
+// LET - ERRO
+console.log('b = ', b);
+let b = 2;
+console.log('b = ', b);
