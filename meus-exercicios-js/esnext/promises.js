2edbe3f8c47f7c01cfc0e851f0366f9a77f20bd8
Thiago Costa <thiago_8777@hotmail.com>
2018-05-10 01:01:09 -0300

    Ecmascript novidades

---
+++ meus-exercicios-js/esnext/promises.js
@@ -0,0 +1,14 @@
+function falarDepoisDe(segundos, frase) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(frase) // Resove chama o then
+        }, segundos * 1000)
+    })
+}
+
+falarDepoisDe(3, 'Que Legal')
+    .then(frase => frase.concat('?!?'))
+    .then(outraFrase => console.log(outraFrase))
+    .then(outraFrase => console.log(outraFrase))
+    .catch(e => console.log(e))
+    
